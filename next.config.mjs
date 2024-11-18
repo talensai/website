83f2c3502004699800d1d49dcdc7f8861cf8c29d
Jonath-z <jonathanzihindula95@gmail.com>
2024-11-18 12:16:42 +0200

    feat: add ICP setup

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const staticBuildEnabled = process.env.STATIC_BUILD_ENABLED === "true";
 const nextConfig = {
+  output: staticBuildEnabled ? "export" : undefined,
   images: {
-    
+
   },
 };
 
